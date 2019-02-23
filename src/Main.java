18d9adcfe0fa21f90e87f22524b21b79bc18dd4d
msys <msys@DESKTOP-5280PHK>
2019-02-23 22:24:54 +0200

    Hi

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	List <Integer> count = new ArrayList <Integer>();
+	List <Integer> price = new ArrayList <Integer>();
+	
+   public Main() {
+		
+	}
+	
+	public void func(int c) {
+		count.add(c);
+	}
+	
+	public void func2(int p) {
+		price.add(p);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
